e1ad81594311313e054416aa3fffbab94fbe866b
hisham8989 <khanhisham25@gmail.com>
2022-04-03 02:31:38 +0530

    serve static file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 const path = require('path')
 
 // serving static files & Config for redering html files
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'views'))
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 app.use(express.static('assets'))
 
 
-app.get('/',require('./routes'))
+app.use('/',require('./routes'))
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
