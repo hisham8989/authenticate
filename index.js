4a92e5f1762f787a9c5a1ea3ae5404027e856d36
hisham8989 <khanhisham25@gmail.com>
2022-04-06 13:58:08 +0530

    settings up

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ require('dotenv').config()
 const port = process.env.PORT || 8000
 const expressLayouts = require('express-ejs-layouts')
 const db = require('./config/mongoose')
-const session = require('express-session')
 const path = require('path')
 
+// use for session cookie
+const session = require('express-session')
+const passport = require('passport')
+const passportLocal = require('./config/passport-local-strategy')
 
 
-// serving static files & Config for redering html files
-app.set('view engine','ejs')
-app.set('views', path.join(__dirname, 'views'))
+
+/** 
+ * Settings for Views and View - helpers
+ */
 app.use(express.urlencoded({extended:true}))
+
+app.use(cookieParser())
+
 app.use(express.static('./assets'))
 
 app.use(expressLayouts)
@@ -21,7 +28,30 @@ app.use(expressLayouts)
 app.set('layout extractStyles',true)
 app.set('layout extractScripts',true)
 
+/** End of Views settings */
+
+
 app.use('/',require('./routes'))
+
+// serving static files & Config for redering html files
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+/** Sessions settings or decrypting middleware */
+
+app.use(session({
+  name:'auth',
+  // TODO changes in secret
+  secret:'menibataunga',
+  saveUninitialized:true,
+  resave:false,
+  cookie:{
+    maxAge:(1000*60*100)
+  }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
