635714d41284960df24288ba7497db1e7c24da77
hisham8989 <khanhisham25@gmail.com>
2022-04-06 12:54:46 +0530

    add passport-local

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -25,8 +25,18 @@ passport.use(
   )
 )
 
-passport.serializeUser(function (user,done) {
-    done()
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log('Error in finding user passport')
+      return done(err)
+    }
+    return done(null, user)
+  })
 })
 
 module.exports = passport
