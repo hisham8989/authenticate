0a2f29fcd80a0ff7e1ad3290f6d18c5c7c12ba83
hisham8989 <khanhisham25@gmail.com>
2022-04-06 12:47:22 +0530

    add passport

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,4 +1,32 @@
-const passport = require('passport');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
 
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+    },
+    function (email, password, done) {
+      User.findOne({ email }, function (err, user) {
+        if (err) {
+          console.log('Error in finding user passport')
+          return done(err)
+        }
+        if (!user) {
+          return done(null, false)
+        }
+        if (!user.verifyPassword(password)) {
+          return done(null, false)
+        }
+        return done(null, user)
+      })
+    }
+  )
+)
+
+passport.serializeUser(function (user,done) {
+    done()
+})
 
 module.exports = passport
