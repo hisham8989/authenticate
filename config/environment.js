4df179022ab6a8c3aa32f2afa59599fe95e60e5f
hisham8989 <khanhisham25@gmail.com>
2022-04-10 22:26:49 +0530

    add environment

--- config/environment.js
+++ config/environment.js
@@ -8,8 +8,19 @@ const development = {
     mail_pass:process.env.MAIL_PASS,
     strategyClientId:process.env.GOOLGLE_CLIENT_ID,
     strategyClientSecret:process.env.GOOLGLE_CLIENT_SECRET,
+    strategyCallbackUrl:"localhost:8000/users/auth/google/callback",
+  }
+const production = {
+    name: 'production',
+    db: 'ninja_users_production',
+    db_host:'hisham',
+    db_pass:process.env.DB_PASSWORD,
+    mail_user:process.env.MAIL_USER,
+    mail_pass:process.env.MAIL_PASS,
+    strategyClientId:process.env.GOOLGLE_CLIENT_ID,
+    strategyClientSecret:process.env.GOOLGLE_CLIENT_SECRET,
     strategyCallbackUrl:process.env.GOOLGLE_CALLBACK_URL,
   }
 
 
-module.exports = development;
+module.exports = eval(process.env.NODE_ENV) == undefined ? development : eval(process.env.NODE_ENV);
